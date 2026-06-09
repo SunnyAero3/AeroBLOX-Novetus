4209f1aa1b9f39019df0182d667cb51e0d9eda6c
SunnyAero3 <jake.hili3231@gmail.com>
2026-06-09 09:52:29 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js"
 
-export const supabase = createClient(
-  "https://zsifxhrjlvwqiuupdxhh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzaWZ4aHJqbHZ3cWl1dXBkeGhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2ODUzNDUsImV4cCI6MjA5NDI2MTM0NX0.epocEGg42xWvA16Moyj6mYUNIm7NW0rvnI5t9NScULg"
-)
+export const supabase = createClient({
+  supabaseUrl: "https://zsifxhrjlvwqiuupdxhh.supabase.co",
+  supabaseKey: "sb_publishable_fbqumnBw-QPjg6mgz30XdQ_92irGYD1"
+})
